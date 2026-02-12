b32b0ce63a35784aebdb078b66b9d64ab1bc69f6
nghoang1288 <54214529+nghoang1288@users.noreply.github.com>
2026-02-12 21:05:15 +0700

    fix: Add serverExternalPackages to resolve runtime __dirname error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
+  serverExternalPackages: ['@prisma/client', 'bcryptjs', 'pg'],
+  experimental: {
     serverActions: {
       bodySizeLimit: '200mb',
     },
