bafd88e3a75344fa75b39739c8026a43a78c08d9
nghoang1288 <54214529+nghoang1288@users.noreply.github.com>
2026-02-12 19:52:55 +0700

    fix: Replace next.config.ts with next.config.mjs and bundle config

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['@prisma/client', 'bcryptjs', 'pg'],
+    experimental: {
+    serverActions: {
+      bodySizeLimit: '200mb',
+    },
+  },
+};
+
+export default nextConfig;
